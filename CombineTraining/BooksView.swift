68e1034c1f46b2c8cd5ef1d0f6e748c14391838d
vitorgebrin <vitorgebrin@gmail.com>
2024-09-12 20:45:38 +0200

    added search
    Added the ability to search with the search bar

--- CombineTraining/BooksView.swift
+++ CombineTraining/BooksView.swift
@@ -12,13 +12,36 @@ import Combine
 class BooksViewModel: ObservableObject {
     @Published var books:[Book] = []
     @Published var hasErrors = false
-    @Published var error: BookError?
-    @Published private(set) var isLoading = false
-    var cancellables = Set<AnyCancellable>()
+    @Published private var error: BookError?
+    @Published private(set) var isLoading:Bool?
+    @Published var textFieldText:String = ""
+    @Published var textIsValid:Bool = false
     
-    func fetchBooks() -> () {
-        let bookUrlString = "https://openlibrary.org/search.json?q=the+lord+of+the+rings&sort=currently_reading&limit=4&lang=en"
+    var cancellables = Set<AnyCancellable>()
+    init(){
+        textFieldSubscriber()
+    }
+    func textFieldSubscriber(){
+        $textFieldText.debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map{ (text) -> Bool in
+                if text.count > 3 {
+                    self.fetchBooks(text)
+                    return true
+                }
+                return false
+            }
+            .sink(receiveValue: { [weak self] isValid in
+                self?.textIsValid = isValid
+            })
+            .store(in: &cancellables)
+    }
+    
+    
+    func fetchBooks(_ string:String) -> () {
+        let bookUrlString = "https://openlibrary.org/search.json?q=\(string.replacingOccurrences(of: " ", with: "+"))&sort=currently_reading&limit=4&lang=en"
+        
         if let url = URL(string: bookUrlString) {
+            self.isLoading = true
             URLSession.shared
                 .dataTaskPublisher(for: url)
                 .receive(on: DispatchQueue.main)
@@ -29,13 +52,15 @@ class BooksViewModel: ObservableObject {
                         print("oops")
                         print(error.localizedDescription)
                     default:
+                        self.isLoading = false
                         break
                     }
                 } receiveValue: { [weak self] bookJson in
                     self?.books = bookJson.docs
+                    
                 }.store(in: &cancellables)
-            
         }
+        
     }
     
     enum BookError {
@@ -48,23 +73,32 @@ struct BooksView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
     @StateObject var vm = BooksViewModel()
-
+    
     var body: some View {
         NavigationSplitView {
-            if !vm.books.isEmpty{
-                ScrollView{
-                    
-                    ForEach(vm.books,id: \.id_amazon){ book in
-                    
-                        BookCard(coverUrl: "https://covers.openlibrary.org/b/id/\(book.cover_i).jpg", title: book.title, author: book.author_name[0], previewText: book.first_sentence[0])
-                    }}.padding()
-                } else {ProgressView()}
-            
+            VStack{
+                TextField("Search...", text: $vm.textFieldText)
+                    .padding(.leading)
+                    .frame(height: 65)
+                    .font(.headline)
+                    .background(Color(uiColor: .systemGray5)).clipShape(RoundedRectangle(cornerRadius: 10))
+                Spacer()
+                if let isLoading = vm.isLoading{
+                    if isLoading{
+                        ScrollView{
+                            ForEach(vm.books,id: \.id_amazon){ book in
+                                
+                                BookCard(coverUrl: "https://covers.openlibrary.org/b/id/\(book.cover_i).jpg", title: book.title, author: book.author_name[0], previewText: book.first_sentence[0])
+                            }
+                        }} else{
+                            ProgressView()
+                            Spacer()
+                        }
+                            }
+            }.padding()
         } detail: {
             Text("Find Book")
-        }.onAppear(perform:{
-            vm.fetchBooks()
-        })
+        }
     }
 }
 
